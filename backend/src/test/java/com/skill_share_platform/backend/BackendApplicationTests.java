3cc141e48df181f36db875b0c5438bbc464ea51b
AvishkaMalith <121160065+Avishka2288123095@users.noreply.github.com>
2025-03-20 18:25:36 +0530

    Initialized the project
    Both frontend and backend is initialized with its technologies.

---
+++ backend/src/test/java/com/skill_share_platform/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skill_share_platform.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
