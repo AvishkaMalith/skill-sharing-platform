06054e983eab32144c5d28615f96adf7057580fa
AvishkaMalith <avishkamalith21@gmail.com>
2025-04-30 08:23:54 +0530

    Changed the App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import ProfilePage from "./components/ProfilePage";
+import NavBar from "./components/NavBar";
 
 const App = () => {
 
   return (
     <>
+      <NavBar />
       <ProfilePage />
     </>
   );
