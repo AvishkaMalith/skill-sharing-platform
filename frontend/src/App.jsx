0b7864f2153d84af1a39e0ac66230c9076748d00
Hesh-k <it22589804@my.sliit.lk>
2025-05-13 19:51:11 +0530

    post wall created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import ViewPost from './pages/ViewPost';
+import PostWall from './pages/PostWall';
 import LearningGoals from './pages/LearningGoals';
 import LearningGoalDetail from './pages/LearningGoalDetail';
 
@@ -12,6 +13,8 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/create-post" element={<CreatePost />} />
                 <Route path="/view-post/:postId" element={<ViewPost />} />
+                <Route path="/wall" element={<PostWall />} />
+                <Route path="/post/:postId" element={<ViewPost />} />
                 <Route path="/learning-goals" element={<LearningGoals />} />
                 <Route path="/learning-goals/:id" element={<LearningGoalDetail />} />
             </Routes>
